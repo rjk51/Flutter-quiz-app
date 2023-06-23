edc4201273aa95b343b4e18c062d43f01b995d99
Ayaan Rajak <77317609+rjk51@users.noreply.github.com>
2023-06-23 23:44:29 +0530

    Add files via upload

---
+++ lib/background_color.dart
@@ -0,0 +1,33 @@
+// import 'package:flutter/material.dart';
+
+// import 'package:quiz_app/first_page.dart';
+
+// const startAlignment = Alignment.topCenter;
+// const endAlignment = Alignment.bottomCenter;
+
+// class BackgroundColor extends StatelessWidget {
+//   const BackgroundColor(this.color1, this.color2, {super.key});
+
+//   const BackgroundColor.purple({super.key})
+//     : color1 = Colors.purple,
+//       color2 = Colors.deepPurple;
+
+//   final Color color1;
+//   final Color color2;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: [color1, color2],
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: FirstPage(),
+//       )
+//     );
+//   }
+// }
